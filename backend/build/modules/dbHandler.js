603420885ee26fa0560e398dd5325a9e3fd1578d
OscarRatcliffe <cyberskely@gmail.com>
2024-08-22 14:05:44 +0100

    Created authCheck function

--- backend/build/modules/dbHandler.js
+++ backend/build/modules/dbHandler.js
@@ -1,6 +1,8 @@
+// Librarys
 import pg from 'pg';
-const { Client } = pg;
+// Custom libraries
 // Connect to DB
+const { Client } = pg;
 const client = new Client({
     user: "postgres",
     password: "postgres",
@@ -9,9 +11,20 @@ const client = new Client({
     database: "postgres"
 });
 // Test Func
-async function getStudents() {
+async function authCheck(username, password, type) {
     await client.connect();
-    const res = await client.query("SELECT * FROM students;");
-    return res;
+    const passwordRes = await client.query(`SELECT password FROM ${type.toLowerCase()} WHERE username='${username}'`); //Get password and its salt fields for given username
+    console.log(passwordRes);
+    // if (passwordRes.rowCount != 1) {
+    //     throw error("ERROR: Repeated username") //Throw repeated user error to help with debugging
+    // } else {
+    //     const hashedPassword = passwordRes.rows[0].password 
+    //     //Check password using bcrypt
+    //     bcrypt.compare(password, hashedPassword.then((res: any) => {
+    //         return res;
+    //     }))
+    // }
+    await client.end();
+    return null;
 }
-export default getStudents;
+export default authCheck;
--- backend/src/modules/dbHandler.ts
+++ backend/src/modules/dbHandler.ts
@@ -1,7 +1,10 @@
+// Librarys
 import pg from 'pg'
-const { Client } = pg
+import bcrypt from 'bcryptjs'
+import { error } from 'console'
 
 // Connect to DB
+const { Client } = pg
 const client = new Client({
     user: "postgres",
     password: "postgres",
@@ -11,13 +14,51 @@ const client = new Client({
 })
 
 // Test Func
-async function getStudents() {
+async function authCheck(username:string, password:string, type:userGroup): Promise<number | null> { //HTTP code for login | Broken func
 
     await client.connect()
 
-    const res = await client.query("SELECT * FROM students;")
-    return res
+    const passwordRes = await client.query(`SELECT password FROM ${type.toLowerCase()} WHERE username='${username}'`) //Get password and its salt fields for given username
 
+    console.log(passwordRes)
+
+    // Custom error handling
+    if (typeof(passwordRes.rowCount) == null) {
+
+        throw error("ERROR: SQL Response malformed") // Used in testing
+        return 500 //Internal Server Error - Used in production
+
+    } else if (passwordRes.rowCount == 0) {
+
+        return 404 //Not found, username not in DB
+
+    } else if (passwordRes.rowCount !> 1) {
+
+        throw error("ERROR Username duplicate - Check Sign Up method")
+        
+    } else {
+
+        // Hash entered password and check against DB
+        const hashedPassword = passwordRes.rows[0].password 
+
+        //Check password using bcrypt
+        bcrypt.compare(password, hashedPassword.then((res: any) => {
+
+            if (res) {
+
+                return 200 //Ok, password correct
+
+            } else {
+
+                return 401 //Unauthorized, password incorrect
+
+            }
+
+        }))
+    }
+
+    await client.end()
+    return null
 }
 
-export default getStudents;
+export default authCheck;
