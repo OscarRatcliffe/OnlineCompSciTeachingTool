5ad54dcc4808732056a7733c29cf888eeca431a6
OscarRatcliffe <cyberskely@gmail.com>
2024-08-27 18:35:10 +0100

    Cookie improvement frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,19 @@
+'use client'
 import styles from "./page.module.scss";
+import cookie from 'cookie';
 
 export default function Home() {
 
-  console.log("Test")
+
+  //Check for login cookie
+  let cookiesParsed = cookie.parse(document.cookie); 
+  
+  if(cookiesParsed.sessionID == undefined) {
+
+    window.location.href = "/login" //If no cookie go to login page
+
+  }
+
 
   return (
     <div className={styles.main}>
